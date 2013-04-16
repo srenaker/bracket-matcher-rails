d2a833b8c4ebdc0d5fbbc60c3f025912e8628bab
Steve Renaker <renaker@yahoo.com>
2013-04-16 08:17:26 -0700

    Added bracket matching algorithm to rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BracketMatcher::Application.config.secret_token = '0ce436dbe4e5d8112b9c8950ef37bbd1d4aa7b5eaab42d39397d9d5b6333c26a7aa9cd09f80a2ede71006773469ae146657010ca13c644270c7d2da8e8e0cac3'
